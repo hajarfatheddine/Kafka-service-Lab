65eb5b1936208f0d4793a716c8315e414d780428
hajar fatheddine <fatheddinehajar@gmail.com>
2023-01-19 18:07:21 +0100

    Data Analytics Real Time Stream Processing

--- src/main/java/com/kafka/kafkalab/services/PageEventService.java
+++ src/main/java/com/kafka/kafkalab/services/PageEventService.java
@@ -7,9 +7,12 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
@@ -43,13 +46,15 @@ public class PageEventService {
             return input;
         };
     }
+    @Bean
     public  Function<KStream<String,PageEvent>,KStream<String,Long>> kStreamFunction(){
         return(input)->{
             return input.filter((k,v)->v.getDuration()>100)
                     .map((k,v)->new KeyValue<>(v.getName(),0L))
                     .groupBy((k,v)->k,Grouped.with(Serdes.String(), Serdes.Long()))
-                    .count()
-                    .toStream();
+                    .windowedBy(TimeWindows.of(Duration.ofMillis(5000)))
+                    .count(Materialized.as("page-count"))
+                    .toStream().map((k,v)->new KeyValue<>("=>"+ k.window().startTime()+k.window().endTime()+k.key(),v));
         };
     }
 }
